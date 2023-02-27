6f5557b45c7bc49830d84ecf368e4cb110b7d090
Anastasia <agolovanyova@yandex.ru>
2023-02-27 14:12:51 +0300

    add Задача 21

---
+++ Задача21/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Math;
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double x1 = 7;
+double y1 = -5;
+double z1 = 0;
+double x2 = 1;
+double y2 = -1;
+double z2 = 9;
+
+double A = x2 - x1;
+double B = y2 - y1;
+double C = z2 - z1;
+
+double result = Sqrt((A * A + B * B + C * C));
+Console.WriteLine(result);
